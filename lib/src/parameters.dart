8292002983a1e8d8da3beb8012915c067932668a
ibotva <etobibillingsley@gmail.com>
2023-07-07 01:31:14 -0400

    Feat: PortmanteausParams & FuzzyRhymeParams

--- lib/src/parameters.dart
+++ lib/src/parameters.dart
@@ -1,3 +1,4 @@
+
 import 'package:rhymebrain/bin/iso6391.dart';
 
 /// This is a list of [_validFunctions] within RhymeBrain.com
@@ -40,7 +41,7 @@ abstract class RhymeBrainParams {
 }
 
 /// Parameters class for getRhymes function.
-class GetRhymeParams extends RhymeBrainParams {
+class RhymeParams extends RhymeBrainParams {
   String lang;
   int? maxResults;
 
@@ -51,7 +52,7 @@ class GetRhymeParams extends RhymeBrainParams {
   ///   maxResults?: 30
   /// )
   /// ```
-  GetRhymeParams({
+  RhymeParams({
     required String word,
     this.lang = "en",
     this.maxResults,
@@ -60,66 +61,50 @@ class GetRhymeParams extends RhymeBrainParams {
   }
 
   /// Returns [Map] that HTTP lib can read.
+  
   toMap() {
     return _toMap(maxResults: maxResults, lang: lang);
   }
 }
 
-class getWordInfo extends RhymeBrainParams {
+class WordInfoParams extends RhymeBrainParams {
   String lang;
 
-  getWordInfo({required String word, this.lang = "en"})
+  WordInfoParams({required String word, this.lang = "en"})
       : super(function: _validFunctions[1], word: word) {
     _langWarningHandler(lang);
   }
+
+  /// Returns [Map] that HTTP lib can read.
+  
+  toMap() {
+    return _toMap(lang: lang);
+  }
 }
 
-/// Parameters class following
-class RhymeBrainParameters {
-  /// [word] you would like to query to RhymeBrain.com
-  String word;
-
-  /// [lang] you would like to have results queried in, represented as ISO369-1
-  String? lang;
-
-  /// [String] function name you would like to call to RhymeBrain.com
-  String function;
-
-  /// Max [int] number of results you would like return
+class PortmanteausParams extends RhymeBrainParams {
+  String lang;
   int? maxResults;
 
-  /// [RhymeBrainParameters] constructor to create parameters for RhymeBrain.
-  RhymeBrainParameters(
-      {required this.word,
-      required this.function,
-      this.lang,
-      this.maxResults}) {
-    /// Checks if the function is listed in RhymeBrain API Documentation and warns if not.
-    if (!(function == "getRhymes") &&
-        !(function == "getWordInfo") &&
-        !(function == "getPortmanteaus")) {
-      print(
-          "[WARNING]: You have not passed a valid function, this may lead to unexpected behavior.");
-    }
-
-    /// Checks if given language code is valid according to ISO639-1 and warns if not.
-    if (lang != null) {
-      if (!iso6391codes.containsKey(lang)) {
-        print(
-            "[WARNING]: You have chosen a language that does not fall standard with ISO639-1 language code, they are two letter codes.");
-      }
-    }
+  PortmanteausParams(
+      {required this.lang, this.maxResults, required String word})
+      : super(function: _validFunctions[2], word: word) {
+    _langWarningHandler(lang);
   }
 
-  /// Converts object to a map readable as URL parameters for HTTP library.
+  /// Returns [Map] that HTTP lib can read.
+  
   toMap() {
-    Map<String, dynamic> map = {"function": function, "word": word};
-    if (maxResults != null) {
-      map["maxResults"] = maxResults!;
-    }
-    if (lang != null) {
-      map["lang"] = lang!;
-    }
-    return map;
+    return _toMap(lang: lang, maxResults: maxResults);
+  }
+}
+
+class FuzzyRhymeParams extends RhymeBrainParams {
+  FuzzyRhymeParams({required String word})
+      : super(function: _validFunctions[3], word: word);
+    
+  
+  Map<String, dynamic> toMap() {
+    return _toMap();
   }
 }
